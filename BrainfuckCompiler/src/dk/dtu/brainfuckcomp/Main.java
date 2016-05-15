874d3f887e9169a0eab573139fc926596dc8e984
Jeppe Enslev <jev@umwelt.dk>
2016-05-15 23:28:27 +0200

    Add dynamic changable brainfuck

--- BrainfuckCompiler/src/dk/dtu/brainfuckcomp/Main.java
+++ BrainfuckCompiler/src/dk/dtu/brainfuckcomp/Main.java
@@ -1,12 +1,22 @@
 package dk.dtu.brainfuckcomp;
 
+import java.util.Scanner;
+
 public class Main {
-	
-	
+
 	public static void main(String[] args) {
 		Brainfuck bf = new Brainfuck(7);
 		
-		String str = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+		String str;
+		Scanner sc = new Scanner(System.in);
+		
+        System.out.println("Enter Brainfuck (Leave blank for \"Hello World!\"):");
+		str = sc.nextLine();
+		if (str.equals("")) {
+			str = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+		}
+		
+		sc.close();
 				
 		try {
 			bf.parse(str);
